667798f00127158dce3f68afc5d52ebf164339ce
SumanthReddy1228 <sumanth0129@gmai.com>
2025-04-20 19:02:27 +0530

    spring data jpa

--- src/main/java/com/sumanth/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/sumanth/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
@@ -1,11 +1,12 @@
 package com.sumanth.springboot.learn_jpa_hibernate.course;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.sumanth.springboot.learn_jpa_hibernate.course.Course;
-import com.sumanth.springboot.learn_jpa_hibernate.jpa.CourseJpaRepository;
+import com.sumanth.springboot.learn_jpa_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
@@ -13,21 +14,36 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 //    @Autowired
 //    private CourseJdbcRepository courseJdbcRepository;
 
+//	@Autowired
+//    private repository repository;
+
 	@Autowired
-    private CourseJpaRepository courseJpaRepository;
-    
+	private CourseSpringDataJpaRepository repository;
+	
     @Override
     public void run(String... args) throws Exception {
-    	courseJpaRepository.insertCourse(new Course(1,"Java","sumanth"));
-    	courseJpaRepository.insertCourse(new Course(2,"python","sam"));
-    	courseJpaRepository.insertCourse(new Course(3,"Spring JPA","ranga"));
-        courseJpaRepository.insertCourse(new Course(4,"AWS","Reddy"));
+    	repository.save(new Course(1,"Java","sumanth"));
+    	repository.save(new Course(2,"python","sam"));
+    	repository.save(new Course(3,"Spring JPA","ranga"));
+        repository.save(new Course(4,"AWS","Reddy"));
         System.out.println("Data inserted successfully");
   
-        courseJpaRepository.deleteCourseById(1);
+        repository.deleteById(1l);
         System.out.println("course deleted");
         System.out.println("get course by id");
-        System.out.println(courseJpaRepository.findCourseById(3));
+        System.out.println(repository.findById(3l));
+        System.out.println("All courses");
+        List<Course> courses=repository.findAll();
+        for(Course course:courses) {
+        	System.out.println(course);
+        }
+        
+        System.out.println();
+        System.out.println("Find By Author");
+        System.out.println(repository.findByAuthor("sam"));
+        System.out.println();
+        System.out.println("Find By course Name");
+        System.out.println(repository.findByName("AWS"));
     }
     
 
---
+++ src/main/java/com/sumanth/springboot/learn_jpa_hibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,14 @@
+package com.sumanth.springboot.learn_jpa_hibernate.course.springdatajpa;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sumanth.springboot.learn_jpa_hibernate.course.Course;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long>{
+	
+	List<Course> findByAuthor(String author);
+	List<Course> findByName(String name);
+
+}
